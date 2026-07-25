0f254c975f8d961e6ccd26bf3045793f73d65892
pattychattyeve-max <patty.chattyeve@gmail.com>
2026-07-26 00:27:57 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "patty-star-astro-v2";
+const CACHE = "patty-star-astro-v3";
 const ASSETS = ["./", "./index.html", "./libs.js", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
